be092609c6f092705eee47eac03cdccad12cb4d3
xjxliuxing <xjxliuxing@163.com>
2023-04-22 13:29:23 +0800

    update version

--- app/src/main/java/android/apphelper/dimens/MainActivity.kt
+++ app/src/main/java/android/apphelper/dimens/MainActivity.kt
@@ -1,18 +1,11 @@
-package android.apphelper.dimens;
+package android.apphelper.dimens
 
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.apphelper.demens.test;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        test.main("sdfdfsd");
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
--- demens/src/main/java/com/apphelper/demens/test.java
+++
@@ -1,7 +0,0 @@
-package com.apphelper.demens;
-
-public class test {
-    public static String main(String args) {
-        return args;
-    }
-}
