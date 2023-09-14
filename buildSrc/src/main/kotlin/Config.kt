479012f3b2062d8b4fbcaa12ee24140cfd41d64c
xjxliuxing@163.com <xjxliuxing@163.com>
2023-09-14 18:04:47 +0800

    update files

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -2,6 +2,6 @@ object Config {
     const val compileSdk = 30
     const val minSdk = 24
     const val targetSdk = 29
-    const val versionCode = 43
-    const val versionName = "4.3.0.4"
+    const val versionCode = 1
+    const val versionName = "1.0"
 }
