6ef8514242c050c8cb13370d56f38afc1785a236
xjxliuxing@163.com <xjxliuxing@163.com>
2023-09-15 18:15:14 +0800

    update plugin

--- resDimens/build.gradle.kts
+++ resDimens/build.gradle.kts
@@ -1,9 +1,7 @@
 plugins {
     id("com.android.library")
-//    id("maven-publish")
+    id("maven-publish")
 }
-
-apply<PublishPlugin>()
 
 group = "com.github.jitpack"
 version = "1.0"
@@ -36,34 +34,44 @@ android {
     }
 }
 
-//val VERSION = latestGitTag().ifEmpty { Config.versionName }
-//
-///**
-// * 获取 git 仓库中最新的 tag作为版本号
-// */
-//fun latestGitTag(): String {
-//    val process = ProcessBuilder("git", "describe", "--tags", "--abbrev=0").start()
-//    return process.inputStream.bufferedReader()
-//        .use { bufferedReader ->
-//            bufferedReader.readText()
-//                .trim()
-//        }
-//}
+publishing { // 发布配置
+    publications {// 发布内容
+        create<MavenPublication>("release") {// 注册一个名字为 release 的发布内容
+            groupId = "com.android.apphelper"
+            artifactId = getModelNameForNamespace()// 插件名称
+            version = latestGitTag().ifEmpty { Config.versionName } // 版本号
 
-//publishing { // 发布配置
-//    publications {// 发布内容
-//        create<MavenPublication>("release") {// 注册一个名字为 release 的发布内容
-//            groupId = "com.android.apphelper"
-//            artifactId = "dimens"// 插件名称
-//            version = "VERSION" // 版本号
-//            afterEvaluate {// 在所有的配置都完成之后执行
-//                // 从当前 module 的 release 包中发布
-//                from(components["release"])
-//            }
-//        }
-//    }
-//}
-//
-//publishing{
-//
-//}
+            afterEvaluate {// 在所有的配置都完成之后执行
+                // 从当前 module 的 release 包中发布
+                from(components["release"])
+            }
+        }
+    }
+}
+
+/**
+ * 获取model的name
+ */
+fun getModelNameForNamespace(): String {
+    var result = "default"
+    android.namespace?.let {
+        if (it.contains(".")) {
+            result = it.split(".")
+                .reversed()
+                .first()
+        }
+    }
+    return result
+}
+
+/**
+ * 获取 git 仓库中最新的 tag作为版本号
+ */
+fun latestGitTag(): String {
+    val process = ProcessBuilder("git", "describe", "--tags", "--abbrev=0").start()
+    return process.inputStream.bufferedReader()
+        .use { bufferedReader ->
+            bufferedReader.readText()
+                .trim()
+        }
+}
