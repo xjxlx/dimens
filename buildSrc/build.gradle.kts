860d76973b30dcdb518160397952b4595e3b70ef
xjxliuxing@163.com <xjxliuxing@163.com>
2023-09-17 00:34:14 +0800

    update plugin2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     `kotlin-dsl`
-    id("maven-publish")
+//    id("maven-publish")
     kotlin("jvm") version "1.6.21"
 }
 
-group = "com.github.jitpack"
-version = "1.0"
+//group = "com.github.jitpack"
+//version = "1.0"
 
 repositories {
     google()
@@ -13,12 +13,12 @@ repositories {
     mavenCentral()
 }
 
-buildscript {
-    dependencies {
-        // 发布JitPack的依赖版本，【3.0+以上用1.5】，【4.1+以上用2.0】，【4.6+以上用2.1】
-        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
-    }
-}
+//buildscript {
+//    dependencies {
+//        // 发布JitPack的依赖版本，【3.0+以上用1.5】，【4.1+以上用2.0】，【4.6+以上用2.1】
+//        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
+//    }
+//}
 
 dependencies {
     implementation("com.android.tools.build:gradle-api:7.4.0")
