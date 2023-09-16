18a623acb31451383310f6a33fc8d6ac9d0ca4c2
xjxliuxing@163.com <xjxliuxing@163.com>
2023-09-17 00:24:32 +0800

    update plugin2

--- resDimens/build.gradle.kts
+++ resDimens/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("maven-publish")
 }
 
-apply<PublishPlugin>()
+//apply<PublishPlugin>()
 
 android {
     namespace = "com.apphelper.dimens"
@@ -25,4 +25,53 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+}
+
+publishing { // 发布配置
+    publications {// 发布内容
+        create<MavenPublication>("release") {// 注册一个名字为 release 的发布内容
+            groupId = "com.android.apphelper"
+            artifactId = getModelNameForNamespace()// 插件名称
+            version = latestGitTag().ifEmpty { Config.versionName } // 版本号
+
+            afterEvaluate {// 在所有的配置都完成之后执行
+                // 从当前 module 的 release 包中发布
+                from(components["release"])
+            }
+        }
+    }
+}
+
+/**
+ * 获取model的name
+ */
+fun getModelNameForNamespace(): String {
+    var result = "default"
+    android.namespace?.let {
+        if (it.contains(".")) {
+            result = it.split(".")
+                .reversed()
+                .first()
+        }
+    }
+    return result
+}
+
+/**
+ * 获取 git 仓库中最新的 tag作为版本号
+ */
+fun latestGitTag(): String {
+    val process = ProcessBuilder("git", "describe", "--tags", "--abbrev=0").start()
+    return process.inputStream.bufferedReader()
+        .use { bufferedReader ->
+            bufferedReader.readText()
+                .trim()
+        }
 }
