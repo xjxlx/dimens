043c354b82ef12ec439bfcbf0ce8d179f49a9c5f
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-05 14:28:12 +0800

    update version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,17 +10,23 @@ pluginManagement {
         mavenLocal()
         mavenCentral()
     }
+
+//    // 配置本地插件
+//    plugins {
+//        id("io.github.xjxlx.publish") version "1.0.9"
+//    }
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-//    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+//    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
     repositories {
         maven { setUrl("https://maven.aliyun.com/repository/public") }
         maven { setUrl("https://maven.aliyun.com/repository/google") }
         maven { setUrl("https://maven.aliyun.com/repository/central") }
         maven { setUrl("https://jitpack.io") }
         google()
+        mavenLocal()
         mavenCentral()
     }
 }
