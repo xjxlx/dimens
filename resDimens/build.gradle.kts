2e30005a4f7589060e65ca7d6af8b3703683ee2c
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-08 11:07:13 +0800

    update version

--- resDimens/build.gradle.kts
+++ resDimens/build.gradle.kts
@@ -1,9 +1,10 @@
+
 plugins {
     id("com.android.library")
     id("kotlin-android")
     id("maven-publish")
+    id("io.github.xjxlx.publish") version "1.4.4"
 }
-//apply<com.android.plugin.plugin.PublishPlugin>()
 
 group = "com.github.xjxlx"
 version = "1.0"
@@ -29,52 +30,60 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
-    publishing {
-        singleVariant("release") {
-            // withSourcesJar()
-            //  withJavadocJar()
-        }
-    }
+//    publishing {
+//        singleVariant("release") {
+//            // withSourcesJar()
+//            //  withJavadocJar()
+//        }
+//    }
 }
 
-afterEvaluate {// 在所有的配置都完成之后执行
-    publishing { // 发布配置
-        publications {
-            register<MavenPublication>("release") {// 注册一个名字为 release 的发布内容
-                groupId = "com.github.xjxlx"
-                artifactId = getModelNameForNamespace()  // 插件名称
-                version = latestGitTag().ifEmpty { "master-SNAPSHOT" } // 版本号
+//afterEvaluate {// 在所有的配置都完成之后执行
+//    publishing { // 发布配置
+//        publications {
+//            register<MavenPublication>("release") {// 注册一个名字为 release 的发布内容
+//                groupId = "com.github.xjxlx"
+//                artifactId = getModelNameForNamespace()  // 插件名称
+//                version = latestGitTag().ifEmpty { "master-SNAPSHOT" } // 版本号
+//
+//                // 从当前 module 的 release 包中发布
+//                from(components["release"])
+//            }
+//        }
+//    }
+//}
 
-                // 从当前 module 的 release 包中发布
-                from(components["release"])
-            }
-        }
-    }
-}
+///**
+// * 获取model的name
+// */
+//fun getModelNameForNamespace(): String {
+//    var result = "default"
+//    android.namespace?.let {
+//        if (it.contains(".")) {
+//            result = it.split(".")
+//                .reversed()
+//                .first()
+//        }
+//    }
+//    return result
+//}
+//
+///**
+// * 获取 git 仓库中最新的 tag作为版本号
+// */
+//fun latestGitTag(): String {
+//    val process = ProcessBuilder("git", "describe", "--tags", "--abbrev=0").start()
+//    return process.inputStream.bufferedReader()
+//        .use { bufferedReader ->
+//            bufferedReader.readText()
+//                .trim()
+//        }
+//}
 
-/**
- * 获取model的name
- */
-fun getModelNameForNamespace(): String {
-    var result = "default"
-    android.namespace?.let {
-        if (it.contains(".")) {
-            result = it.split(".")
-                .reversed()
-                .first()
-        }
-    }
-    return result
-}
+apply<com.android.helper.plugin.PublishPlugin>()
 
-/**
- * 获取 git 仓库中最新的 tag作为版本号
- */
-fun latestGitTag(): String {
-    val process = ProcessBuilder("git", "describe", "--tags", "--abbrev=0").start()
-    return process.inputStream.bufferedReader()
-        .use { bufferedReader ->
-            bufferedReader.readText()
-                .trim()
-        }
-}
+//configure<com.android.helper.plugin.PublishPluginExtension> {
+//    groupId.set("com.github.xjxlx")
+//    artifactId.set("dimens")
+//    version.set("v2.0.0.8")
+//}
