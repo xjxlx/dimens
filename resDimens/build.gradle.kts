ab83005b986e5f09abc4ce3d991fac453aa3d273
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-11 16:38:47 +0800

    update version

--- resDimens/build.gradle.kts
+++ resDimens/build.gradle.kts
@@ -1,14 +1,12 @@
-import com.android.helper.plugin.PublishPlugin
-
 plugins {
     id("com.android.library")
     id("kotlin-android")
     id("maven-publish")
 
-    id("io.github.xjxlx.publish")
+//    id("io.github.xjxlx.publish")
 }
 
-apply<PublishPlugin>()
+//apply<com.android.helper.plugin.PublishPlugin>()
 
 group = "com.github.xjxlx"
 version = "1.0.0"
@@ -33,19 +31,23 @@ android {
 //        sourceCompatibility = JavaVersion.VERSION_11
 //        targetCompatibility = JavaVersion.VERSION_11
 //    }
+
+    publishing {
+        singleVariant("release")
+    }
 }
 
-//afterEvaluate {// 在所有的配置都完成之后执行
-//    publishing { // 发布配置
-//        publications {
-//            register<MavenPublication>("release") {// 注册一个名字为 release 的发布内容
-//                groupId = "com.github.xjxlx"
-//                artifactId = getModelNameForNamespace()  // 插件名称
-//                version = latestGitTag().ifEmpty { "master-SNAPSHOT" } // 版本号
-//
-//                // 从当前 module 的 release 包中发布
-//                from(components["release"])
-//            }
-//        }
-//    }
-//}
+afterEvaluate {// 在所有的配置都完成之后执行
+    publishing { // 发布配置
+        publications {
+            register<MavenPublication>("release") {// 注册一个名字为 release 的发布内容
+                groupId = "com.github.xjxlx"
+                artifactId = "dimens"  // 插件名称
+                version = "1.0.1"  // 版本号
+
+                // 从当前 module 的 release 包中发布
+                from(components["release"])
+            }
+        }
+    }
+}
