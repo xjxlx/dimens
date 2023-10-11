7c51dd3146c722fa39a6290abd88e150f815aac3
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-11 16:48:41 +0800

    update version

--- resDimens/build.gradle.kts
+++ resDimens/build.gradle.kts
@@ -27,10 +27,10 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
-//    compileOptions {
-//        sourceCompatibility = JavaVersion.VERSION_11
-//        targetCompatibility = JavaVersion.VERSION_11
-//    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
 
     publishing {
         singleVariant("release")
