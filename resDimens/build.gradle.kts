b1d023cb3f9a27b460f752636848b495eef27103
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-12 22:19:20 +0800

    update version

--- resDimens/build.gradle.kts
+++ resDimens/build.gradle.kts
@@ -43,11 +43,24 @@ afterEvaluate {// 在所有的配置都完成之后执行
             register<MavenPublication>("release") {// 注册一个名字为 release 的发布内容
                 groupId = "com.github.xjxlx"
                 artifactId = "dimens"  // 插件名称
-                version = "1.0.1"  // 版本号
+                version = latestGitTag()  // 版本号
 
                 // 从当前 module 的 release 包中发布
                 from(components["release"])
             }
         }
     }
+}
+
+
+/**
+ * 获取 git 仓库中最新的 tag作为版本号
+ */
+fun latestGitTag(): String {
+    val process = ProcessBuilder("git", "describe", "--tags", "--abbrev=0").start()
+    return process.inputStream.bufferedReader()
+        .use { bufferedReader ->
+            bufferedReader.readText()
+                .trim()
+        }
 }
