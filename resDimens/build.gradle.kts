6edb1ce8d54c7b0d7b8e0f764cb1c26cd1f84f76
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-12 23:31:42 +0800

    update version

--- resDimens/build.gradle.kts
+++ resDimens/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     id("com.android.library")
     id("kotlin-android")
-    id("maven-publish")
+//    id("maven-publish")
 
-//    id("io.github.xjxlx.publish")
+    id("io.github.xjxlx.publish")
 }
 
-//apply<com.android.helper.plugin.PublishPlugin>()
+apply<com.android.helper.plugin.PublishPlugin>()
 
 //group = "com.github.jitpack"
 //version = "1.0"
@@ -33,26 +33,25 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
-    publishing {
-        singleVariant("release")
-    }
+//    publishing {
+//        singleVariant("release")
+//    }
 }
 
-afterEvaluate {// 在所有的配置都完成之后执行
-    publishing { // 发布配置
-        publications {
-            register<MavenPublication>("release") {// 注册一个名字为 release 的发布内容
-                groupId = "com.github.xjxlx"
-                artifactId = "dimens"  // 插件名称
-                version = latestGitTag()  // 版本号
-
-                // 从当前 module 的 release 包中发布
-                from(components["release"])
-            }
-        }
-    }
-}
-
+//afterEvaluate {// 在所有的配置都完成之后执行
+//    publishing { // 发布配置
+//        publications {
+//            register<MavenPublication>("release") {// 注册一个名字为 release 的发布内容
+//                groupId = "com.github.xjxlx"
+//                artifactId = "dimens"  // 插件名称
+//                version = latestGitTag()  // 版本号
+//
+//                // 从当前 module 的 release 包中发布
+//                from(components["release"])
+//            }
+//        }
+//    }
+//}
 
 /**
  * 获取 git 仓库中最新的 tag作为版本号
