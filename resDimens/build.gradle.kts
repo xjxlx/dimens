d5814dbe3ecf07c6fba00b5465a83c35de2054d3
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-14 23:37:19 +0800

    update files

--- resDimens/build.gradle.kts
+++ resDimens/build.gradle.kts
@@ -1,10 +1,14 @@
 plugins {
     id("com.android.library")
     id("kotlin-android")
-    id("io.github.xjxlx.publish")
+    id("maven-publish")
+//    id("io.github.xjxlx.publish")
 }
 
-apply<com.android.helper.plugin.PublishPlugin>()
+//apply<com.android.helper.plugin.PublishPlugin>()
+
+//group = "com.github.jitpack"
+//version = "1.0"
 
 android {
     namespace = "com.apphelper.dimens"
@@ -27,4 +31,35 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    publishing {
+        singleVariant("release")
+    }
+}
+
+afterEvaluate {// 在所有的配置都完成之后执行
+    publishing { // 发布配置
+        publications {
+            register<MavenPublication>("release") {// 注册一个名字为 release 的发布内容
+                groupId = "com.github.xjxlx"
+                artifactId = "dimens"  // 插件名称
+                version = latestGitTag()  // 版本号
+
+                // 从当前 module 的 release 包中发布
+                from(components["release"])
+            }
+        }
+    }
+}
+
+/**
+ * 获取 git 仓库中最新的 tag作为版本号
+ */
+fun latestGitTag(): String {
+    val process = ProcessBuilder("git", "describe", "--tags", "--abbrev=0").start()
+    return process.inputStream.bufferedReader()
+        .use { bufferedReader ->
+            bufferedReader.readText()
+                .trim()
+        }
 }
