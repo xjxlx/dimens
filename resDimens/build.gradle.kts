87a04309b12d7948d2a9169d43d9ae338b42d5fa
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-23 21:56:36 +0800

    update files

--- resDimens/build.gradle.kts
+++ resDimens/build.gradle.kts
@@ -26,5 +26,25 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    publishing {
+        singleVariant("release")
+    }
 }
 
+group = "com.github.jitpack"
+version = "1.0"
+
+afterEvaluate {// 在所有的配置都完成之后执行
+    publishing { // 发布配置
+        publications {
+            register<MavenPublication>("release") {// 注册一个名字为 release 的发布内容
+                groupId = "com.github.xjxlx"
+                artifactId = "dimens"  // 插件名称
+                version = "1.0.3"  // 版本号
+                // 从当前 module 的 release 包中发布
+                from(components["release"])
+            }
+        }
+    }
+}
