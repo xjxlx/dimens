9cbf27c42b011c568b8e42117bdc5c5192f2e574
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-23 11:19:58 +0800

    update files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 buildscript {
-
     // 从本地插件读取
     repositories {
         maven { setUrl("https://plugins.gradle.org/m2/") }
@@ -12,13 +11,19 @@ buildscript {
         // 发布JitPack的依赖版本，【3.0+以上用1.5】，【4.1+以上用2.0】，【4.6+以上用2.1】
         classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
         // classpath("com.android.tools.build:gradle:7.4.2")
-        // classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+        classpath(libs.com.android.tools.build.gradle)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
     }
 }
-
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    id("com.android.application") version "7.4.2" apply false
-    id("com.android.library") version "7.4.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-    id("org.jetbrains.kotlin.jvm") version "1.7.0" apply false
+    // id("com.android.application") version "7.4.2" apply false
+    alias(libs.plugins.com.android.application) apply false
+    // id("com.android.library") version "7.4.2" apply false
+    alias(libs.plugins.com.android.library) apply false
+    // id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    // id("org.jetbrains.kotlin.jvm") version "1.7.0" apply false
+    alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
 }
+true
