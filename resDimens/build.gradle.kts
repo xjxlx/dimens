6a2e35709a28e414aea0ac4e4fb45059bd725d5a
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-24 09:54:49 +0800

    update files

--- resDimens/build.gradle.kts
+++ resDimens/build.gradle.kts
@@ -1,9 +1,10 @@
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     id("com.android.library")
     id("kotlin-android")
-    id("maven-publish")
-//    id("io.github.xjxlx.publish") version "1.0.2"
-//    id("io.github.xjxlx.catalog") version "1.0.2"
+    // id("maven-publish")
+     id("io.github.xjxlx.publish")
+     id("io.github.xjxlx.catalog")
 }
 
 android {
@@ -28,24 +29,24 @@ android {
 //        targetCompatibility = JavaVersion.VERSION_11
 //    }
 
-    publishing {
-        singleVariant("release")
-    }
+//    publishing {
+//        singleVariant("release")
+//    }
 }
 
-group = "com.github.jitpack"
-version = "1.0"
-
-afterEvaluate {// 在所有的配置都完成之后执行
-    publishing { // 发布配置
-        publications {
-            register<MavenPublication>("release") {// 注册一个名字为 release 的发布内容
-                groupId = "com.github.xjxlx"
-                artifactId = "dimens"  // 插件名称
-                version = "1.0.3"  // 版本号
-                // 从当前 module 的 release 包中发布
-                from(components["release"])
-            }
-        }
-    }
-}
+//group = "com.github.jitpack"
+//version = "1.0"
+//
+//afterEvaluate {// 在所有的配置都完成之后执行
+//    publishing { // 发布配置
+//        publications {
+//            register<MavenPublication>("release") {// 注册一个名字为 release 的发布内容
+//                groupId = "com.github.xjxlx"
+//                artifactId = "dimens"  // 插件名称
+//                version = "1.0.3"  // 版本号
+//                // 从当前 module 的 release 包中发布
+//                from(components["release"])
+//            }
+//        }
+//    }
+//}
